c290477642376b747a90a9cb2db3842efd32a1e6
Kim Pevey <kcpevey@openteams.com>
2026-07-07 14:11:27 +0000

    Prepare 0.1.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.0] - 2026-07-07
+
 ### Added
 
 - Torchvision image-classification wrapper (`modelmaite.image_classification.TorchvisionICModel`) that exposes torchvision models as MAITE-compatible image-classification models.
--- src/modelmaite/__init__.py
+++ src/modelmaite/__init__.py
@@ -1,7 +1,11 @@
 """modelmaite: model utilities built on the maite protocols."""
 
+from importlib.metadata import version
+
 from modelmaite.image_classification import OnnxICModel, TorchvisionICModel
 from modelmaite.object_detection import DetectionTarget, OnnxODModel, TorchvisionODModel, VisdroneODModel
+
+__version__ = version("modelmaite")
 
 __all__ = [
     "DetectionTarget",
@@ -10,4 +14,5 @@ __all__ = [
     "TorchvisionICModel",
     "TorchvisionODModel",
     "VisdroneODModel",
+    "__version__",
 ]
